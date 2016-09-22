8d62464aedbce8106ef81114b4cf81a280515514
Mouzhai <wangpengfei@shmingjiang.sh.cn>
2016-09-22 18:54:25 +0800

    初次提交

---
+++ myfirstapp/src/main/java/com/shmj/mouzhai/myfirstapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.shmj.mouzhai.myfirstapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
