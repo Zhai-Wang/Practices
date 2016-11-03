1e6b43bac09609e8415bab6fb508818e5b3024a6
Mouzhai <wangpengfei@shmingjiang.sh.cn>
2016-11-03 10:30:06 +0800

    初次提交

---
+++ myfirstapp/src/main/java/com/shmj/mouzhai/myfirstapp/DisplayMessageActivity.java
@@ -0,0 +1,25 @@
+package com.shmj.mouzhai.myfirstapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setText(message);
+        textView.setTextSize(40);
+
+        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_message);
+        layout.addView(textView);
+    }
+}
