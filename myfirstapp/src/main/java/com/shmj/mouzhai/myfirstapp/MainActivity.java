0219da7c544c2ad2997cf1a94273268b349a5e3c
Mouzhai <wangpengfei@shmingjiang.sh.cn>
2016-11-04 16:05:04 +0800

    Activity 生命周期体验

--- myfirstapp/src/main/java/com/shmj/mouzhai/myfirstapp/MainActivity.java
+++ myfirstapp/src/main/java/com/shmj/mouzhai/myfirstapp/MainActivity.java
@@ -16,15 +16,65 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setUpActionBar();
+
+        //noinspection StatementWithEmptyBody
+        if (savedInstanceState != null){
+            //获取存储的值
+        }else {
+            //设置默认值
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //Activity 销毁时存储信息
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        //如果 Bundle 对象存在，继续往下执行
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-
+        //确保所有资源得到释放，停止一切活动
         android.os.Debug.stopMethodTracing();
     }
 
+    /**
+     * send 按钮的点击事件
+     * 发送 Intent
+     *
+     * @param view 可操作的 View 对象
+     */
     public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.edit_message);
@@ -33,6 +83,9 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    /**
+     * 判断当前版本是否支持控件
+     */
     private void setUpActionBar() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
             android.app.ActionBar actionBar = getActionBar();
