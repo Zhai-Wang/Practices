e2d4b457721508405389167de55079e70fb4ab76
Mouzhai <wangpengfei@shmingjiang.sh.cn>
2016-11-18 16:56:53 +0800

    进入通讯录，选择联系人

--- festivalsms/src/main/java/com/shmj/mouzhai/festivalsms/SendMsgActivity.java
+++ festivalsms/src/main/java/com/shmj/mouzhai/festivalsms/SendMsgActivity.java
@@ -1,23 +1,34 @@
 package com.shmj.mouzhai.festivalsms;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.shmj.mouzhai.festivalsms.Bean.Festival;
 import com.shmj.mouzhai.festivalsms.Bean.FestivalLab;
 import com.shmj.mouzhai.festivalsms.Bean.Msg;
 import com.shmj.mouzhai.festivalsms.view.FlowLayout;
 
+import java.util.HashSet;
+import java.util.zip.Inflater;
+
 public class SendMsgActivity extends AppCompatActivity {
 
     public static final String FESTIVAL_ID = "festivalId";
     public static final String MSG_ID = "msgId";
+    private static final int REQUEST_CODE = 1;
     private int mFestivalId;
     private int mMsgId;
     private EditText etMsg;
@@ -27,6 +38,9 @@ public class SendMsgActivity extends AppCompatActivity {
     private View layoutLoading;
     private Festival festival;
     private Msg msg;
+    private HashSet<String> mContactNames = new HashSet<>();
+    private HashSet<String> mContactNums = new HashSet<>();
+    private LayoutInflater mInflater;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +49,21 @@ public class SendMsgActivity extends AppCompatActivity {
 
         initData();
         initView();
+        initEvent();
+    }
+
+    private void initEvent() {
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
     }
 
     private void initView() {
+        mInflater = LayoutInflater.from(this);
         etMsg = (EditText) findViewById(R.id.et_content);
         btnAdd = (Button) findViewById(R.id.btn_add);
         flContracts = (FlowLayout) findViewById(R.id.fl_contracts);
@@ -57,6 +83,52 @@ public class SendMsgActivity extends AppCompatActivity {
         mMsgId = getIntent().getIntExtra(MSG_ID, -1);
         festival = FestivalLab.getInstance().getFestivalById(mFestivalId);
         setTitle(festival.getName());
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == REQUEST_CODE){
+            if (resultCode == RESULT_OK){
+                Uri contactUri = data.getData();
+                Cursor cursor = getContentResolver().query(contactUri, null, null, null, null);
+                cursor.moveToFirst();
+                String contactName = cursor.getString(cursor.getColumnIndex
+                        (ContactsContract.Contacts.DISPLAY_NAME));
+                String contactNum = getContactNum(cursor);
+                if (!TextUtils.isEmpty(contactNum)){
+                    mContactNames.add(contactName);
+                    mContactNums.add(contactNum);
+                    addTag(contactName);
+                }
+            }
+        }
+    }
+
+    private void addTag(String contactName) {
+        TextView textView = (TextView) mInflater.inflate(R.layout.tag, flContracts, false);
+        textView.setText(contactName);
+        flContracts.addView(textView);
+    }
+
+    private String getContactNum(Cursor cursor) {
+        int numberCount = cursor.getInt(cursor.getColumnIndex
+                (ContactsContract.Contacts.HAS_PHONE_NUMBER));
+        String number = null;
+        if (numberCount > 0){
+            int contactId = cursor.getInt(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+            Cursor phoneCursor = getContentResolver().query
+                    (ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + contactId,
+                            null, null);
+            phoneCursor.moveToFirst();
+            number = phoneCursor.getString(phoneCursor.getColumnIndex
+                    (ContactsContract.CommonDataKinds.Phone.NUMBER));
+            phoneCursor.close();
+        }
+        cursor.close();
+        return number;
     }
 
     public static void toActivity(Context context, int festivalId, int msgId){
