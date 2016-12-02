0b758c90da9a1305c78d7d42eef5566a3c2450de
Mouzhai <wangpengfei@shmingjiang.sh.cn>
2016-12-02 13:17:18 +0800

    实现多线程下载文件

--- downloaddemo/src/main/java/com/shmj/mouzhai/downloaddemo/MainActivity.java
+++ downloaddemo/src/main/java/com/shmj/mouzhai/downloaddemo/MainActivity.java
@@ -4,13 +4,9 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.shmj.mouzhai.downloaddemo.adapter.FileListAdapter;
